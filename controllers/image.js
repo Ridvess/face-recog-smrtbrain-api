53b70e3da9bbfe0aecf577bafcd24bd1cea45be2
Ridves <phillroman24@gmail.com>
2020-09-15 20:34:28 +0300

    Files ready for push

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+    apiKey: 'fbd29b51793e4a8bb163e54528eb9620'
+});
+
+const handleApiCall = (req,res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage =(db) => (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
